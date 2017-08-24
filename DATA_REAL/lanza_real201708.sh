49a8174ed219c9cc6ef32708effdd0a29e9bea6a
Javi Gonzalez <javiollo@hotmail.com>
2017-08-24 17:43:21 +0200

    Add files via upload

---
+++ DATA_REAL/lanza_real201708.sh
@@ -0,0 +1,20 @@
+python Exporter.py --username "vertiseguros" --since 2017-08-01 --output vertiseguros.csv
+python Exporter.py --querysearch "vertiseguros" --since 2017-08-01 --output vertispotify.csv
+python Exporter.py --querysearch "verti spotify" --since 2017-08-01 --output vertispotify.csv
+python Exporter.py --querysearch "verti parte" --since 2017-08-01 --output verti_parte.csv
+python Exporter.py --querysearch "verti_mejor" --since 2017-08-01 --output verti_mejor.csv
+python Exporter.py --querysearch "verti mal" --since 2017-08-01 --output verti_mal.csv
+python Exporter.py --querysearch "verti gracias" --since 2017-08-01 --output  verti_gracias.csv
+python Exporter.py --querysearch "verti excelente" --since 2017-08-01 --output  verti_excelente.csv
+python Exporter.py --querysearch "verti estafa" --since 2017-08-01 --output  verti_estafa.csv
+python Exporter.py --querysearch "verti coche" --since 2017-08-01 --output  verti_coche.csv
+python Exporter.py --querysearch "verti bueno" --since 2017-08-01 --output  verti_bueno.csv
+python Exporter.py --querysearch "verti bien" --since 2017-08-01 --output  verti_bien.csv
+python Exporter.py --querysearch "verti arroba" --since 2017-08-01 --output  vertiarroba.csv
+python Exporter.py --querysearch "verti accidente" --since 2017-08-01 --output  verti_accidente.csv
+python Exporter.py --querysearch "verti timo" --since 2017-08-01 --output  verti_timo.csv
+python Exporter.py --querysearch "verti seguros" --since 2017-08-01 --output  verti_seguros.csv
+python Exporter.py --querysearch "verti seguro" --since 2017-08-01 --output  verti_seguro.csv
+python Exporter.py --querysearch "verti problema" --since 2017-08-01 --output  verti_problema.csv
+python Exporter.py --querysearch "verti perfecto" --since 2017-08-01 --output  verti_perfecto.csv
+python Exporter.py --querysearch "verti peor" --since 2017-08-01 --output  verti_peor.csv
